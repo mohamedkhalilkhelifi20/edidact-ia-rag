e6fba5fc13e91216da5a3acc17fbed922db0916c
Mohamed Khalil Khelifi <mohamedkhalil.khelifi@polytechnicien.tn>
2026-07-15 10:35:58 +0100

    fix: skip Qdrant server version check to remove warning

--- src/database/client.py
+++ src/database/client.py
@@ -2,4 +2,8 @@ from qdrant_client import QdrantClient
 from src.config.config import QDRANT_URL, QDRANT_API_KEY
 
 def get_client() -> QdrantClient:
-    return QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY)
+    return QdrantClient(
+        url=QDRANT_URL,
+        api_key=QDRANT_API_KEY,
+        check_compatibility=False,
+    )
